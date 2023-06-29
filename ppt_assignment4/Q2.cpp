205cab73b4c6e978e5fcbe44d115118835c1f7bd
adarshpandey327 <130994929+adarshpandey327@users.noreply.github.com>
2023-06-29 23:16:37 +0530

    Q2.cpp

---
+++ ppt_assignment4/Q2.cpp
@@ -0,0 +1,67 @@
+/*
+<aside>
+💡 **Question 2**
+
+Given two **0-indexed** integer arrays nums1 and nums2, return *a list* answer *of size* 2 *where:*
+
+- answer[0] *is a list of all **distinct** integers in* nums1 *which are **not** present in* nums2*.*
+- answer[1] *is a list of all **distinct** integers in* nums2 *which are **not** present in* nums1.
+
+**Note** that the integers in the lists may be returned in **any** order.
+
+**Example 1:**
+
+**Input:** nums1 = [1,2,3], nums2 = [2,4,6]
+
+**Output:** [[1,3],[4,6]]
+
+**Explanation:**
+
+For nums1, nums1[1] = 2 is present at index 0 of nums2, whereas nums1[0] = 1 and nums1[2] = 3 are not present in nums2. Therefore, answer[0] = [1,3].
+
+For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and nums2[2] = 6 are not present in nums2. Therefore, answer[1] = [4,6].
+
+</aside>
+*/
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> findDistinctNumbers(vector<int>& nums1, vector<int>& nums2) {
+    unordered_set<int> set1(nums1.begin(), nums1.end());
+    unordered_set<int> set2(nums2.begin(), nums2.end());
+
+    vector<int> distinct1, distinct2;
+    for (int num : nums1) {
+        if (set2.find(num) == set2.end()) {
+            distinct1.push_back(num);
+        }
+    }
+
+    for (int num : nums2) {
+        if (set1.find(num) == set1.end()) {
+            distinct2.push_back(num);
+        }
+    }
+
+    return {distinct1, distinct2};
+}
+
+int main() {
+    vector<int> nums1 = {1, 2, 3};
+    vector<int> nums2 = {2, 4, 6};
+
+    vector<vector<int>> result = findDistinctNumbers(nums1, nums2);
+
+    cout << "Distinct numbers in nums1: ";
+    for (int num : result[0]) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "Distinct numbers in nums2: ";
+    for (int num : result[1]) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
