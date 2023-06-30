586978f5e3109c524e160a678bfbe2ff55958b4c
adarshpandey327 <130994929+adarshpandey327@users.noreply.github.com>
2023-06-30 22:25:54 +0530

    Update Q4.cpp

--- ppt_assignment5/Q4.cpp
+++ ppt_assignment5/Q4.cpp
@@ -23,5 +23,45 @@ For nums2, nums2[0] = 2 is present at index 1 of nums1, whereas nums2[1] = 4 and
 
 </aside>
 */
-//code_
+#include <bits/stdc++.h>
+using namespace std;
+vector<vector<int>> findDistinctNumbers(vector<int>& nums1, vector<int>& nums2) {
+    unordered_set<int> set1(nums1.begin(), nums1.end());
+    unordered_set<int> set2(nums2.begin(), nums2.end());
 
+    vector<int> distinct1, distinct2;
+    for (int num : nums1) {
+        if (set2.find(num) == set2.end()) {
+            distinct1.push_back(num);
+        }
+    }
+
+    for (int num : nums2) {
+        if (set1.find(num) == set1.end()) {
+            distinct2.push_back(num);
+        }
+    }
+
+    return {distinct1, distinct2};
+}
+
+int main() {
+    vector<int> nums1 = {1, 2, 3};
+    vector<int> nums2 = {2, 4, 6};
+
+    vector<vector<int>> result = findDistinctNumbers(nums1, nums2);
+
+    cout << "Distinct numbers in nums1: ";
+    for (int num : result[0]) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    cout << "Distinct numbers in nums2: ";
+    for (int num : result[1]) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
