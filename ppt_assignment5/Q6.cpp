5d7f1fc92fbcaa289f76decff9c816a252550ec0
adarshpandey327 <130994929+adarshpandey327@users.noreply.github.com>
2023-06-30 23:04:52 +0530

    Q6.cpp

---
+++ ppt_assignment5/Q6.cpp
@@ -0,0 +1,36 @@
+/*
+<aside>
+💡 **Question 6**
+
+Given an integer array nums of length n where all the integers of nums are in the range [1, n] and each integer appears **once** or **twice**, return *an array of all the integers that appears **twice***.
+
+You must write an algorithm that runs in O(n) time and uses only constant extra space.
+
+**Example 1:**
+
+**Input:** nums = [4,3,2,7,8,2,3,1]
+
+**Output:**
+
+[2,3]
+
+</aside>
+*/
+//code-
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++)
+        {
+            int index= abs(nums[i])-1;
+            if(nums[index]<0)
+                ans.push_back(index + 1);
+
+            nums[index]= (nums[index]* (-1));
+        }
+        return ans;
+    }
+};
+
+//https://leetcode.com/problems/find-all-duplicates-in-an-array/submissions/907189003/
